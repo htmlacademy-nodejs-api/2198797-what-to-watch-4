df80176ecb0189725901e40afb86d0e41cac43c2
Vitalii Beliaev <vtaliy.belyaev@gmail.com>
2023-05-15 17:37:38 +0300

    Добавил получение данных с сервера и чтение/запись с помощью stream

--- src/core/cli-command/helper.command.ts
+++ src/core/cli-command/helper.command.ts
@@ -5,16 +5,6 @@ export default class HelpCommand implements CliCommandInterface {
   public readonly name = '--help';
 
   public async execute(): Promise<void> {
-    /*console.log(`
-        Программа для подготовки данных для REST API сервера.
-        Пример:
-            main.js --<command> [--arguments]
-        Команды:
-            --version:                   # выводит номер версии
-            --help:                      # печатает этот текст
-            --import <path>:             # импортирует данные из TSV
-            --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных
-        `);*/
     console.log(chalk.green('Программа для подготовки данных для REST API сервера.'));
     console.log(chalk.yellow('Пример:'));
     console.log(chalk.blue('  main.js --<command> [--arguments]'));
