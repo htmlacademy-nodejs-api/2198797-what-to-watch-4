3014d2373db9286c8c54a4b2a3982ebc4ac1efc9
Vitalii Beliaev <vtaliy.belyaev@gmail.com>
2023-06-26 10:58:16 +0300

    Удалил getPassword, исправил отступ в movies.rdo.ts

--- src/modules/movie/rdo/movies.rdo.ts
+++ src/modules/movie/rdo/movies.rdo.ts
@@ -2,28 +2,28 @@ import { Expose, Type } from 'class-transformer';
 import UserRdo from '../../user/rdo/user.rdo.js';
 
 export default class MoviesRdo {
-    @Expose()
+  @Expose()
   public id!: string;
 
-    @Expose()
-    public name!: string;
+  @Expose()
+  public name!: string;
 
-    @Expose()
-    public premiereDate!: Date;
+  @Expose()
+  public premiereDate!: Date;
 
-    @Expose()
-    public genre!: string;
+  @Expose()
+  public genre!: string;
 
-    @Expose()
-    public previewVideoLink!: string;
+  @Expose()
+  public previewVideoLink!: string;
 
-    @Expose()
-    public posterImage!: string;
+  @Expose()
+  public posterImage!: string;
 
-    @Expose()
-    public commentsCount!: string;
+  @Expose()
+  public commentsCount!: string;
 
-    @Expose({ name: 'userId'})
-    @Type(() => UserRdo)
-    public user!: UserRdo;
+  @Expose({ name: 'userId' })
+  @Type(() => UserRdo)
+  public user!: UserRdo;
 }
